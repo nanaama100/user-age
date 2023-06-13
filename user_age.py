3dccac6f5d3f1e6ab126f9815051d6a0cd5d1b03
Nana Ama Asantewaa Adu <136357748+nanaama100@users.noreply.github.com>
2023-06-13 03:17:42 +0000

    Add files via upload

---
+++ user_age.py
@@ -0,0 +1,13 @@
+age = int(input("enter your age: "))
+current_year = int(input("enter current year: "))
+year_of_birth = current_year - age 
+print("Year of Birth = ", year_of_birth)
+if age < 18: 
+    print("You are Ineligible to Vote")
+else:
+    print("You are Eligible to Vote")
+
+
+    
+
+
